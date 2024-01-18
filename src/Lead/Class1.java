2472ad2cda829f3fad3aedd6818580e6fcfc8277
faruk <faruk@farukalam.lan>
2024-01-18 18:39:26 -0500

    hi this alam please start working

---
+++ src/Lead/Class1.java
@@ -0,0 +1,13 @@
+package Lead;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a=10;
+   int b=20;
+   int ad=a+b;
+   System.out.println(ad);
+	}
+
+}
